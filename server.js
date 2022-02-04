70d12a67fe8d83385e42c7785c59ebe076ac813c
import <import.hanbat@gmail.com>
2022-02-04 14:18:11 +0900

    connect db

--- server.js
+++ server.js
@@ -3,6 +3,16 @@ const app = express();
 const bodyParser = require('body-parser');
 const MongoClient = require('mongodb').MongoClient;
 
+var db;
+MongoClient.connect('mongodb+srv://import:import1015@cluster0.a1cx0.mongodb.net/HomePage?retryWrites=true&w=majority', { useUnifiedTopology: true }, function (err, client) {
+	if (err) return console.log(err)
+	db = client.db('HomePage');
+
+	app.listen(8080, function () {
+		console.log('listening on 8080')
+	});
+});
+
 app.use(express.urlencoded({ extended: true }))
 
 app.set('view engine', 'ejs');
@@ -11,12 +21,7 @@ app.set('view engine', 'ejs');
 app.use('/css',express.static('css')); //미들웨어
 app.use('/fontawesome-free-5.15.4-web',express.static('fontawesome-free-5.15.4-web')); //미들웨어
 app.use('/images',express.static('images'));
-MongoClient.connect('mongodb+srv://import:import1015@cluster0.a1cx0.mongodb.net/HomePage?retryWrites=true&w=majority',function(err, client){
-    if(err) return console.log(err);
-    app.listen(8080, function() {
-        console.log('listening on 8080');
-    })
-})
+
 
 //메인 홈페이지
 app.get('/', function(req,res){
@@ -86,26 +91,24 @@ app.get('/write',function(req,res){
 /* db 업로드 코드 */
 
 app.post('/add',function(req,res){
-    // console.log(req.body.title);
-    // console.log(req.body.category);
+
+
+    console.log(req.body.title);
+    console.log(req.body.category);
     console.log(req.body.content);
-
-
-    // db.collection('counter').findOne({name : 'web'},function(err, resu){
-    //     console.log(resu.totalPosts);
-    //     var totalpostnum = resu.totalPosts;
-    //     db.collection('post').insertOne({_id : totalpostnum + 1,제목 : req.body.title, 카테고리 : req.body.category, 내용: req.body.content}, function(err,resu){
-    //         console.log('저장완료');
-    //     }); 
-    //     //counter라는 콜렉션에 있는 totalPost라는 항목도 1이 증가해야됨.(수정)
-    //     db.collection('counter').updateOne({name : 'web'},{$inc :{totalPosts:1}},function(err, resu){
-    //         if(err){return console.log(err)}; 
-    //     });//수정을 할 때 쓰는 함수(한개만 가능, 여러개는 updateMany로 쓰면 됨)
-    // });//updateOne({/* 어떤 데이터를 수정할지 */},{/* 수정 값 */},function(){})
+    db.collection('counter').findOne({name: req.body.category},function(err,resu){
+        console.log(resu.totalPosts);
+        var totalpostnum = resu.totalPosts;
+        db.collection(req.body.category).insertOne({title: req.body.title, category:req.body.category, content: req.body.content}, function(err,resu){
+            console.log('저장완료');
+        });
+        db.collection('counter').updateOne({name: req.body.category},{$inc :{totalPosts:1}},function(err, reus){
+            if(err){return console.log(err)};
+        })
+    })
     
+    //counter라는 콜렉션에 있는 totalPost라는 항목도 1이 증가해야됨.(수정)
     
-    
-
 })
 
 
